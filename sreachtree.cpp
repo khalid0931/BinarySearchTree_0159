05f97921333d072d6efdd3b768c4299dc709ee5d
khalid0931 <mutiarif53@gmail.com>
2026-06-09 10:52:29 +0700

    membuat void inorder

--- sreachtree.cpp
+++ sreachtree.cpp
@@ -75,5 +75,21 @@ public:
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 };
 
