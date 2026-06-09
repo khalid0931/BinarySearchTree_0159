36434c4eb1d2da0d3cc6c0882488a1c359906b0b
khalid0931 <mutiarif53@gmail.com>
2026-06-09 10:56:43 +0700

    membuat case 4-5

--- sreachtree.cpp
+++ sreachtree.cpp
@@ -169,6 +169,26 @@ int main()
             break;
         }
 
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+
+        case '5':
+            return 0;
+
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+
+    return 0;
+}
+
 
 
 
