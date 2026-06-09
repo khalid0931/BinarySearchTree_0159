f2f4f74a04dbad256745c05d08dde9193b381faf
khalid0931 <mutiarif53@gmail.com>
2026-06-09 10:51:40 +0700

    membuat void search yg benar

--- sreachtree.cpp
+++ sreachtree.cpp
@@ -56,5 +56,10 @@ public:
             parent->rightchild = newNode;
         }
     }
+
+    // This function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, Node *&parent, Node *&currentNode)
+    
 };
 
